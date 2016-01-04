8a52be15219f9d0575628caaa21a9e81b872a996
Steinsplitter <steinsplitter-wiki@live.com>
2016-01-04 12:33:09 +0100

    start
    start

---
+++ index.php
@@ -0,0 +1,99 @@
+tools.steinsplitter@tools-bastion-02:~/public_html/renaming$ cat index.php
+<?php
+/**
+ * @author Steinsplitter / https://commons.wikimedia.org/wiki/User:Steinsplitter
+ * @copyright 2015 tool authors
+ * @license http://unlicense.org/ Unlicense
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+        <title>Media requiring renaming</title>
+        <link rel="stylesheet" href="/steinsplitter/bootstrap.css">
+    <style>
+      body {
+        padding-top: 60px;
+      }
+    </style>
+<?php if (isset($_GET['re'])): ?>
+<meta http-equiv="cache-control" content="no-cache" />
+<script language="javascript">
+window.onload = function(start) {
+(function countdown(remaining) {
+    if(remaining === 1)
+        location.reload(true);
+    document.getElementById('countdown').innerHTML = remaining;
+    setTimeout(function(){ countdown(remaining - 1); }, 1000);
+})(60)
+};
+</script>
+<?php endif; ?>
+</head>
+<body>
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="index.php">Media requiring renaming</a>
+          <div class="nav-collapse collapse">
+                <ul id="toolbar-right" class="nav pull-right">
+               <li><a href="//commons.wikimedia.org/wiki/Category:Media_requiring_renaming">Commonscat</a></li>
+               </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="container">
+<?php
+// Stats
+$hi = ( "new.txt" );
+$hii = file( $hi );
+$hii[0] ++;
+$fp = fopen( $hi , "w" );
+fputs( $fp , "$hii[0]" );
+fclose( $fp );
+?>
+<p>This is a fast loading and easy to use interface for file renamers who are working on the queue. Only files from the maincat show up here.</p>
+<span><a href="#" onclick="location.reload(true); return false;" class ="btn btn-primary btn-success">Refresh<div id="countdown"></div></a>
+<?php if (isset($_GET['re'])): ?>
+<a href="index.php?stop=yes" class ="btn btn-primary btn-inverse">Autorefresh off</a>
+<?php else: ?>
+<a href="index.php?re=ok" class ="btn btn-primary btn-info">Autorefresh on</a>
+<?php endif; ?>
+</span><br><br>
+<?php
+$tools_pw = posix_getpwuid ( posix_getuid () );
+$tools_mycnf = parse_ini_file( $tools_pw['dir'] . "/replica.my.cnf" );
+$db = new mysqli( 'commonswiki.labsdb', $tools_mycnf['user'], $tools_mycnf['password'], 'commonswiki_p' );
+if ( $db->connect_errno )
+        die( "Failed to connect to labsdb: (" . $db->connect_errno . ") " . $db->connect_error );
+$r = $db->query( 'SELECT REPLACE(page_title, \'_\', \' \'), rev_user_text, DATE_FORMAT(rev_timestamp, \'%H:%i:%s %b %d %Y\') FROM page JOIN categorylinks ON page_id=cl_from JOIN revision ON page_latest = rev_id AND page_is_redirect=0 AND cl_to=\'Media_requiring_renaming\' and page_namespace = \'6\'' );
+$num = $r->num_rows;
+unset( $tools_mycnf, $tools_pw );
+$data = date('H:i:s (m-d-Y)', time());
+echo '<p>There are currently <b>'.$num.'</b> move requests in the queue.</p><p>Data as of '.$data.' (UTC).';
+if ( $num == 0 ) {
+     echo "<div class=\"alert alert-info\"><b>No requests:</b> There are zero filemove requests. No backlog. Cool :-).</div>";
+}
+//header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+?>
+
+<table align="left" class="table table-hover" style ="text-align: left;">
+<?php while ( $row = $r->fetch_row() ):?>
+ <tr>
+  <td width="84">
+  <img class="dfile" src="//commons.wikimedia.org/w/thumb.php?f=<?= htmlspecialchars( urlencode ( $row[0] ) ) ?>&amp;w=80&amp;p=40">
+  </td>
+  <td>
+  <a href="//commons.wikimedia.org/w/index.php?title=File:<?= htmlspecialchars ( urlencode ( $row[0] ) ) ?>">File:<?= str_replace( "_", " ", htmlspecialchars( $row[0] ) ); ?></a><br>(<a href="//commons.wikimedia.org/w/index.php?title=File:<?= htmlspecialchars ( urlencode ( $row[0] ) ) ?>&action=history">History</a> | <a href="//commons.wikimedia.org/w/index.php?title=File:<?= htmlspecialchars ( urlencode ( $row[0] ) ) ?>&action=edit">Edit</a> | <a href="//commons.wikimedia.org/w/index.php?title=Special:Log&page=File:<?= htmlspecialchars ( urlencode ( $row[0] ) ) ?>">Logs</a> | <a href="//commons.wikimedia.org/w/index.php?title=Special%3AGlobalUsage&limit=50&target=<?= htmlspecialchars ( urlencode ( $row[0] ) ) ?>">Usage</a>)<br>
+  Last edited by <a href="//commons.wikimedia.org/w/index.php?title=User:<?= htmlspecialchars ( urlencode ( $row[1] ) ) ?>"><?= htmlspecialchars ( $row[1] ) ?></a> at  <?= htmlspecialchars ( $row[2] ) ?>
+  </td>
+</tr>
+<?php endwhile; ?>
+
+</table>
+</div>
+</div>
+</body>
+</html>
