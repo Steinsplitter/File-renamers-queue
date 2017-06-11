3632bc054141ed886b3e2c5e63f293aa07aec758
framawiki <framawiki@tools.wmflabs.org>
2017-06-11 18:55:37 +0200

    add "len" key in the api

--- index.php
+++ index.php
@@ -30,14 +30,12 @@ if (isset($_GET['api'])) {
 	while ( $row = $r->fetch_row() ) {
 		$elements[] = array('title' => utf8_encode($row[0]), 'user' => utf8_encode($row[1]), 'date' => $row[2]);
 	}
-	
+	$answer['len'] = count($elements);
 	$answer['elements'] = $elements;
 	echo json_encode($answer);
 	header('Content-Type: application/json;charset=utf-8');
 	exit(0);
 }
-
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
